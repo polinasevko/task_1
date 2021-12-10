9d98f7bfebc5494db20c0e1f05652df126261b73
Polina Sevko <polinasevko24@gmail.com>
2021-12-10 11:30:43 +0300

    Create xml/json serializers

---
+++ formats.py
@@ -0,0 +1,35 @@
+from abc import ABC, abstractmethod
+from dicttoxml import dicttoxml
+import json
+
+
+class UploadFormat(ABC):
+    def dump(self, obj, fp):
+        with open(fp, "w") as fp:
+            fp.write(self.dumps(obj))
+
+    @abstractmethod
+    def dumps(self, obj):
+        pass
+
+
+class LoadFormat(ABC):
+    @abstractmethod
+    def load(self, fp):
+        pass
+
+
+class Json(UploadFormat, LoadFormat):
+    def dumps(self, obj):
+        return json.dumps(obj)
+
+    def load(self, fp):
+        with open(fp, 'r') as fp:
+            return json.load(fp)
+
+
+class Xml(UploadFormat):
+    def dumps(self, obj):
+        xml = dicttoxml(obj, custom_root='rooms', attr_type=False, item_func=lambda x: x[:-1])
+        xml_decode = xml.decode()
+        return xml_decode
